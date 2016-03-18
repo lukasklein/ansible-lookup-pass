5bbc372684de31fb65bbdcf9d43293e5cd7e766a
Lukas Klein <lukas@lukasklein.com>
2016-03-18 18:59:14 +0100

    if password is not found, generate a new one

--- pass.py
+++ pass.py
@@ -14,7 +14,12 @@ class LookupModule(LookupBase):
         for term in terms:
             var = term.split()[0]
 
-            out = check_output("pass " + var, shell=True).rstrip()
+            out = check_output("pass {}".format(var), shell=True).rstrip()
+            if 'Error:' in out:
+                # Not found, generate
+                out = check_output("pass generate {} 20".format(var), shell=True).rstrip()
+                out = out.replace("\x1b[1m\x1b[37mThe generated password for \x1b[4m{}\x1b[24m is:\x1b[0m\n\x1b[1m\x1b[93".format(var), "")  # Works better than regex...
+                out = out.replace("\x1b[0m", "")
             result.append(out)
 
         return result
